2edabac0542771cfd81eb81327383bd9211d3924
Synevir <synevir@git_mail.com>
2019-08-21 11:12:42 +0300

    Added check of `mysqldump` command result The case when the renamed database does not exist or syntax error in `$db_old_name`

--- MySQL_scripts/rename_db.sh
+++ MySQL_scripts/rename_db.sh
@@ -4,35 +4,49 @@
 #    script for rename database in MySQL        #
 #    using mysqladmin                           #
 #################################################
+
 user="root"
 host_from="localhost"
 db_old_name="bash"
 db_new_name="bash_4"
 dump_file=$db_old_name.sql
-# temporary dir
-# temp_dir=$HOME
 
-echo $db_old_name
-echo $db_new_name
-echo $LANG
+echo "Old database name:   $db_old_name"
+echo "New database name:   $db_new_name"
+echo "System encoding:     $LANG"
+echo "---------------------------------"
+
+# creating database dump
 mysqldump -u $user -h $host_from $db_old_name > $dump_file
-mysqladmin -u $user -h $host_from create $db_new_name
 result=$?
-
 if [[ $result -ne 0 ]]
   then
-    echo "error \"mysqladmin create $db_new_name\""
+    echo "Result:  error in \`mysqldump $db_new_name\` command!"
+    echo "Script aborted"
     exit $result
   else
-    echo "result \"mysqladmin create database\": Ok"
+    echo "mysqldump result: Ok"
+    echo '--------------------------------------------'
+fi
+
+
+# creating new database from dump
+mysqladmin -u $user -h $host_from create $db_new_name
+result=$?
+if [[ $result -ne 0 ]]
+  then
+    echo "error in \`mysqladmin create $db_new_name\` command"
+    echo
+    exit $result
+  else
+    echo "\`mysqladmin create database\` result: Ok"
+    echo '------------------------------------------'
 fi
 
 mysql -u $user -h $host_from $db_new_name < $dump_file
 
-# визуальная проверка наполнения таблицы в новой базе
-# mysql -u $user -h $host -e "select id from s limit 10" $db_new_name
 
-# rename database in fields priveleges tables
+echo 'rename database in fields priveleges tables:'
 tables_privileges="db tables_priv columns_priv host"
 for tbl_name in ${tables_privileges}
   do
