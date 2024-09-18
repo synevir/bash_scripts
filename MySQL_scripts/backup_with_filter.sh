46a0b3076093b5d53a883dca4dcec8d7ab9d93f3
Synevir <synevir@git_mail.com>
2024-09-18 21:49:02 +0300

    Added option to use 7z format for dump compression

--- MySQL_scripts/backup_with_filter.sh
+++ MySQL_scripts/backup_with_filter.sh
@@ -7,10 +7,20 @@
 #                                                                       #
 # Options:                                                              #
 #       -d <database name> to dump                                      #
-#       -p <pattern> tables to dump                                     #
-#       default pattern `LIKE '%'` (dump all tables in database)        #
+#       -p <pattern> tables to dump, default pattern `LIKE '%'`         #
+#                                    (dump all tables in database)      #
+#       -7z use 7z for dump compress                                    #
+#           Warning: 7z pipeline mode don't save file access mode, so   #
+#                    after uncompress dump use `chmod =440` command     #
 #                                                                       #
 #########################################################################
+
+# For example:
+#   backup_with_filter.sh -d some_base -p em
+# will create dump "some_base" database with pattern LIKE'*em*':
+#   mysqldump `some_base` [`em_table` `table_em2` .....] | gzip > dump.sql.gz
+# You can change pattern format in "Read option section"
+
 
 # Original commands has gotten from site highload.today
 #
@@ -26,11 +36,12 @@
 
 
 
-# Default database and pattern for dump
+# Default options for dump:
 db_name='test'
 pattern="%"
 all_tables='n'
 ERROR_LOG='error.log'
+compressor='gzip'
 echo '--------------------------------------------------------'
 
 # Check server availability
@@ -39,6 +50,7 @@ result=$?
 [[ $result -ne 0 ]] && { echo -e ".....script aborted!\n"; exit 0; }
 
 
+# Read options section
 while [ -n "$1" ]; do
   case "$1" in
     -d) db_name="$2"
@@ -47,6 +59,9 @@ while [ -n "$1" ]; do
     -p) pattern="%$2%"
           #echo "Found the -p option, with pattern string: $pattern"
           shift;;
+    -7z) compressor="7z"
+          #echo "Found the -7z option, use 7z format in stream mode"
+          shift;;
     --) shift; break ;;
      *) echo "$1 is not an option";;
   esac
@@ -54,14 +69,15 @@ while [ -n "$1" ]; do
   shift
 done
 
-
-echo "database: $db_name"
-echo " pattern: $pattern"
+echo
+echo "  database: $db_name"
+echo "   pattern: $pattern"
+echo "compressor: $compressor"
 
 
 if [ "$pattern" == "%" ]
 then
-    read -t 10 -p "Empty pattern. Dump all tables? [n]: " all_tables
+    read -t 10 -p "Empty pattern. Dump all tables? y/n [n]: " all_tables
     if [[ "$all_tables" == "n" || "$all_tables" == "N" || "$all_tables" == "" ]]
     then
         echo "....dump canseled"
@@ -69,24 +85,29 @@ then
     fi
 fi
 
-echo "--------------"
+echo "---------------"
 
 
 # Create a list of tables
-echo "Create list of tables to dump"
+echo -n "Create list of tables to dump..."
 mysql -N information_schema -e "SELECT table_name FROM tables WHERE 
 table_schema = '`echo $db_name`' AND table_name LIKE '`echo $pattern`'" > tables.txt 2> $ERROR_LOG
-echo
+echo "Ok!"
 echo "Tables list to dump:"
 cat tables.txt
 
 
 # Create tables dump
-mysqldump `echo $db_name` `cat tables.txt` 2>> $ERROR_LOG | gzip > dump.sql.gz
-# mysqldump `echo $db_name` `cat tables.txt` | 7z a -si -mx5 dump.sql.7z
+case "$compressor" in
+    gzip) mysqldump `echo $db_name` `cat tables.txt` 2>> $ERROR_LOG | gzip > dump.sql.gz ;;
+      7z) mysqldump `echo $db_name` `cat tables.txt` 2>> $ERROR_LOG | 7z a -si -mx5 dump.sql.7z ;;
+      # `7z -si` option works equal `>>` (add), 
+      #  so if you already have `dump.sql.7z` you must delete it before create new dump! 
+esac
 
 rm tables.txt
 echo
-echo "Dump complited."
+echo "Dump complited:"
+ls -o dump.sql*
 echo
 exit 0
