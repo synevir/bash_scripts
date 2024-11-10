4f8e46a8e9a441550174bf62678ea796186a7efa
Synevir <synevir@git_mail.com>
2024-11-10 12:26:29 +0200

    Delete <space> and <Tab> from file

--- Admins_scripts/Truncate_log/rotate_log.sh
+++ Admins_scripts/Truncate_log/rotate_log.sh
@@ -9,7 +9,7 @@
 # Usage:  `./rotate_log.sh`      truncates files from $LOGS variable
 #         `./rotate_log.sh 'log_file_1 log_file_2....'`    trancates
 # files from $LOGS and "log_file_1 log_file_2...." files.
-# 
+#
 ######################################################################
 
 LOGS="$HOME/.bash_history 
@@ -20,13 +20,13 @@ TRUNC_PERSENTAGE=30
 # Checking for additional file to truncate
 [[ -s $1 ]] && LOGS="$LOGS $1"
 
-echo "------   Trancate Ratio = $TRUNC_PERSENTAGE%    ------"
+echo "------   Trancate Ratio = $TRUNC_PERSENTAGE%    ------" #| tee -a $LOG
 
 for FILE in $LOGS
 do
     echo
-    echo -n "FILE \`$FILE\`"    
-    
+    echo -n "FILE: \"$FILE\""
+
     # If file exists and has one or more lines
     if [[ -s $FILE ]]
     then
@@ -34,22 +34,22 @@ do
         echo "---------------------------"
         total_lines=`grep -c $ "$FILE"`
         echo "Contains $total_lines lines."
-        
-        # Calculate number lines to delete    
+
+        # Calculate number lines to delete
         let number_of_lines_to_delete=$total_lines*$TRUNC_PERSENTAGE/100
         echo "Number lines to delete - $number_of_lines_to_delete"
-        echo 'Delete lines.......' 
-        
-        if [[ $number_of_lines_to_delete -ge 1 ]] 
-        then 
+        echo 'Delete lines.......'
+
+        if [[ $number_of_lines_to_delete -ge 1 ]]
+        then
             sed -i '1,'"$number_of_lines_to_delete"'d' $FILE
             [[ $? -eq 0 ]] && echo "$number_of_lines_to_delete lines were deleted" || echo "sed Error!"
-        else 
+        else
             sed -i '1,1d' $FILE
-            [[ $? -eq 0 ]] && echo "1 line was deleted." || echo "sed Error!"            
+            [[ $? -eq 0 ]] && echo "1 line was deleted." || echo "sed Error!"
         fi
     else
-        echo " is not exist or has no lines."        
+        echo " is not exist or has no lines."
     fi
 done
 
